6cc4846c32cf62d1c9ac1a55977f5fcdedb5680d
DaLumberjack <andrewhodel35@gmail.com>
2024-07-31 22:21:16 -0600

    replace with example

--- sensor.py
+++ sensor.py
@@ -1,13 +1,15 @@
 """Platform for sensor integration."""
 from __future__ import annotations
 
-from homeassistant.components.sensor import SensorEntity
-from homeassistant.const import TEMP_CELSIUS
+from homeassistant.components.sensor import (
+    SensorDeviceClass,
+    SensorEntity,
+    SensorStateClass,
+)
+from homeassistant.const import UnitOfTemperature
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
-
-from . import DOMAIN
 
 
 def setup_platform(
@@ -17,37 +19,20 @@ def setup_platform(
     discovery_info: DiscoveryInfoType | None = None
 ) -> None:
     """Set up the sensor platform."""
-    # We only want this platform to be set up via discovery.
-    if discovery_info is None:
-        return
     add_entities([ExampleSensor()])
 
 
 class ExampleSensor(SensorEntity):
-    """Representation of a sensor."""
+    """Representation of a Sensor."""
 
-    def __init__(self) -> None:
-        """Initialize the sensor."""
-        self._state = None
-
-    @property
-    def name(self) -> str:
-        """Return the name of the sensor."""
-        return 'Example Temperature'
-
-    @property
-    def state(self):
-        """Return the state of the sensor."""
-        return self._state
-
-    @property
-    def unit_of_measurement(self) -> str:
-        """Return the unit of measurement."""
-        return TEMP_CELSIUS
+    _attr_name = "Example Temperature"
+    _attr_native_unit_of_measurement = UnitOfTemperature.CELSIUS
+    _attr_device_class = SensorDeviceClass.TEMPERATURE
+    _attr_state_class = SensorStateClass.MEASUREMENT
 
     def update(self) -> None:
         """Fetch new state data for the sensor.
 
         This is the only method that should fetch new data for Home Assistant.
         """
-        self._state = self.hass.data[DOMAIN]['temperature']
+        self._attr_native_value = 23
