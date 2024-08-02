a57ca2f4804986e72f92cb70b1867d2992069abd
Andyroo <andrewhodel35@gmail.com>
2024-08-01 20:24:20 -0600

    remove init funcationality

--- __init__.py
+++ __init__.py
@@ -1,11 +1,1 @@
-
-from __future__ import annotations
-
-from homeassistant.core import HomeAssistant
-from homeassistant.helpers.typing import ConfigType
-from .const import DOMAIN
-
-def setup(hass: HomeAssistant, config: ConfigType) -> bool:
-    hass.data[DOMAIN] = {}
-    hass.states.set("communifarm.Farm", "works!")
-    return True
+"""The Communifarm integration."""
